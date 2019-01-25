5baef30aae746811d1b2107e09d2a67fa30d245b
Richard Hooper <richard.hooper@cloud-trade.com>
2019-01-25 13:14:05 +0000

    Updated Readme

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 output "subnet_id" {
   value = "${azurerm_kubernetes_cluster.k8s.agent_pool_profile.0.vnet_subnet_id}"
 }
@@ -22,28 +21,3 @@ output "docker_bridge_cidr" {
 output "pod_cidr" {
   value = "${azurerm_kubernetes_cluster.k8s.network_profile.0.pod_cidr}"
 }
-=======
-output "subnet_id" {
-  value = "${azurerm_kubernetes_cluster.k8s.agent_pool_profile.0.vnet_subnet_id}"
-}
-
-output "network_plugin" {
-  value = "${azurerm_kubernetes_cluster.k8s.network_profile.0.network_plugin}"
-}
-
-output "service_cidr" {
-  value = "${azurerm_kubernetes_cluster.k8s.network_profile.0.service_cidr}"
-}
-
-output "dns_service_ip" {
-  value = "${azurerm_kubernetes_cluster.k8s.network_profile.0.dns_service_ip}"
-}
-
-output "docker_bridge_cidr" {
-  value = "${azurerm_kubernetes_cluster.k8s.network_profile.0.docker_bridge_cidr}"
-}
-
-output "pod_cidr" {
-  value = "${azurerm_kubernetes_cluster.k8s.network_profile.0.pod_cidr}"
-}
->>>>>>> a8098991b1a2c9df9c14cc277e187e58d5c4990c
